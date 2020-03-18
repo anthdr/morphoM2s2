bf27e973584ddbe349ceda2c9e08be7c852746ba
anthdr <57603673+anthdr@users.noreply.github.com>
2020-03-18 14:28:34 +0100

    Update spanishmodel.py

--- spanishmodel.py
+++ spanishmodel.py
@@ -13,20 +13,19 @@ from sklearn.preprocessing import LabelEncoder
 # load the dataset
 dataset = pandas.read_csv('spanish-paradigm.csv', delimiter=',')
 # split into input (X) and output (y) variables
-X = dataset['last']
 labelencoder = LabelEncoder()
+X = dataset['last']
 X = labelencoder.fit_transform(X)
 X2 = dataset['last2']
 X3 = dataset['last3']
 X4 = dataset['last4']
-y = dataset['regular']
+y = dataset['class']
+y = labelencoder.fit_transform(y)
 
 
 # define the keras model
 model = Sequential()
 model.add(Dense(128, input_dim=1, activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(128, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 # compile the keras model
@@ -42,8 +41,10 @@ print(nfold)
 for train_index,test_index in KFold(nfold).split(X):
     x_train,x_test=X[train_index],X[test_index]
     y_train,y_test=y[train_index],y[test_index]
-    history = model.fit(x_train, y_train, validation_split=0.5, epochs=350, batch_size=30)
+    history = model.fit(x_train, y_train, validation_split=0.5, epochs=100, batch_size=30)
     print('Model evaluation ',model.evaluate(x_test,y_test))
     _, acc = model.evaluate(x_train, y_train, verbose=1)
     #scores.append(acc)
     histories.append(history)
+
+print(history.history.get('accuracy')[-1])
