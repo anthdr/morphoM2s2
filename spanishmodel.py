3f7261e9d47422a3b21622017d72fad85dbbea5d
anthdr <57603673+anthdr@users.noreply.github.com>
2020-03-19 15:28:04 +0100

    Update spanishmodel.py

--- spanishmodel.py
+++ spanishmodel.py
@@ -1,5 +1,8 @@
 import keras
+import sys
 import pandas as pd
+import numpy as np
+np.set_printoptions(threshold=sys.maxsize)
 from keras.models import Sequential
 from keras.layers import Dense
 from keras import optimizers
@@ -10,33 +13,25 @@ from sklearn.feature_extraction.text import CountVectorizer
 # load the dataset
 data = pd.read_csv("origin/spanish-paradigm.csv")
 
-#X = ngrams(data['stem'], 2)
 
 
+cv = CountVectorizer(ngram_range=(2,2),analyzer='char_wb', lowercase=False)
+X = cv.fit_transform(data['stem'])
+X = X.todense()
 
-X = CountVectorizer(data['stem'],ngram_range=(2,2),analyzer='char_wb', lowercase=False)
-X = X.fit_transform(data['stem'])
-
-#vectorizer = CountVectorizer(analyzer='char_wb', ngram_range=(2, 2), min_df=1) 
-#X = vectorizer.fit_transform(data['stem'])
-#vectorizer.get_feature_names()
-#X = pd.DataFrame(X.todense())
-
-X.vocabulary_
-print(X)
-print(type(X))
 
 
 # split into input (X) and output (y) variables
 y = data['class']
-labelencoder = LabelEncoder()
-y = labelencoder.fit_transform(y)
-
+y = pd.get_dummies(y)
+col = y.columns
+y = y.values
+y_count = y.shape[1]
 
 # define the keras model
 model = Sequential()
-model.add(Dense(128, input_dim=X.shape, activation='relu'))
-model.add(Dense(activation='sigmoid'))
+model.add(Dense(64, input_dim=X.shape[1], activation='relu'))
+model.add(Dense(y_count, activation='sigmoid'))
 
 # compile the keras model
 sgd = optimizers.adam(lr=0.01)
@@ -51,10 +46,35 @@ print(nfold)
 for train_index,test_index in KFold(nfold).split(X):
     x_train,x_test=X[train_index],X[test_index]
     y_train,y_test=y[train_index],y[test_index]
-    history = model.fit(x_train, y_train, validation_split=0.2, epochs=350, batch_size=30)
+    history = model.fit(x_train, y_train, validation_split=0.2, epochs=16, batch_size=8)
     print('Model evaluation ',model.evaluate(x_test,y_test))
     _, acc = model.evaluate(x_train, y_train, verbose=1)
     #scores.append(acc)
     histories.append(history)
 
+
+
 print(history.history.get('accuracy')[-1])
+print(history.history.get('val_loss')[-1])
+
+print('\n \n \n \n')
+
+test_dummy = 'acrezent'
+test_dummy = [test_dummy]
+
+test_dummy = cv.fit_transform(test_dummy)
+print(cv.vocabulary_)
+print(type(test_dummy))
+print(test_dummy)
+test_dummy = test_dummy.todense()
+print(type(test_dummy))
+print(test_dummy)
+
+
+fill_value = 0
+test_dummy = np.ones(X.shape)*fill_value
+
+#test = model.predict_classes(test_dummy)
+
+#print(test)
+
