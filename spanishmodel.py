570dd6b4a2e9250ae7e524a9b33993b34b78e5c6
anthdr <57603673+anthdr@users.noreply.github.com>
2020-03-22 17:50:46 +0100

    1

--- spanishmodel.py
+++ spanishmodel.py
@@ -10,27 +10,31 @@ from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import CountVectorizer
 
+
+
 # load the dataset
 data = pd.read_csv("origin/spanish-paradigm.csv")
 
 
-
+#prepare X
 cv = CountVectorizer(ngram_range=(2,2),analyzer='char_wb', lowercase=False)
 X = cv.fit_transform(data['stem'])
 X = X.todense()
 
 
-
-# split into input (X) and output (y) variables
+# prepare y
 y = data['class']
 y = pd.get_dummies(y)
 col = y.columns
 y = y.values
 y_count = y.shape[1]
 
+
+
 # define the keras model
 model = Sequential()
-model.add(Dense(64, input_dim=X.shape[1], activation='relu'))
+model.add(Dense(128, input_dim=X.shape[1], activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dense(y_count, activation='sigmoid'))
 
 # compile the keras model
@@ -41,26 +45,24 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 scores, histories = list(), list()
 nfold = 3
 kfold = KFold(nfold, shuffle=True, random_state=1)
-print('number of fold:')
-print(nfold)
 for train_index,test_index in KFold(nfold).split(X):
-    x_train,x_test=X[train_index],X[test_index]
-    y_train,y_test=y[train_index],y[test_index]
-    history = model.fit(x_train, y_train, validation_split=0.2, epochs=16, batch_size=8)
+    x_train,x_test = X[train_index],X[test_index]
+    y_train,y_test = y[train_index],y[test_index]
+    history = model.fit(x_train, y_train, validation_split=0.2, epochs=32, batch_size=16, verbose=0)
     print('Model evaluation ',model.evaluate(x_test,y_test))
-    _, acc = model.evaluate(x_train, y_train, verbose=1)
+    #_, acc = model.evaluate(x_train, y_train, verbose=1)
     #scores.append(acc)
-    histories.append(history)
+    #histories.append(history)
 
 
-print('\n \n')
+print('\n')
 
 print('accuracy is at:')
 print(history.history.get('accuracy')[-1])
 print('val_loss is at:')
 print(history.history.get('val_loss')[-1])
 
-print('\n \n')
+print('\n')
 
 def test(x):
     namestem = x
