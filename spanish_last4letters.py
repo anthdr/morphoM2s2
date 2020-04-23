cac0b9d2bb27e901b7281af2b7d341cb53105271
anthdr <57603673+anthdr@users.noreply.github.com>
2020-04-23 13:58:20 +0200

    Update spanish_last4letters.py

--- spanish_last4letters.py
+++ spanish_last4letters.py
@@ -40,7 +40,6 @@ X = X.drop('stem', 1)
 X = OneHotEncoder().fit_transform(X)
 
 
-
 # prepare y
 y = data['class']
 y = pd.Series(y).str.replace('-.*', '', regex=True)
@@ -56,12 +55,12 @@ kfold = KFold(n_splits=nfold, shuffle=True, random_state=1)
 for train_index, test_index in kfold.split(X, y):
     # define the keras model
     model = Sequential()
-    model.add(Dense(16, input_dim=X.shape[1], activation='relu'))
-    model.add(Dense(16, activation='relu'))
+    model.add(Dense(32, input_dim=X.shape[1], activation='relu'))
+    model.add(Dense(32, activation='relu'))
     model.add(Dense(y_count, activation='softmax'))
 
     # compile the keras model
-    sgd = optimizers.adam(lr=0.05)
+    sgd = optimizers.adam(lr=0.02)
     model.compile(loss='binary_crossentropy',
                   optimizer='adam', metrics=['accuracy'])
 
@@ -69,9 +68,10 @@ for train_index, test_index in kfold.split(X, y):
     x_train, x_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
     model.fit(x_train, y_train, validation_split=0.2,
-              epochs=16, batch_size=8, verbose=1)
+              epochs=8, batch_size=4, verbose=1)
     cvscores.append(model.evaluate(x_test, y_test))
     print('Model evaluation ', cvscores[-1])
+    print('\n')
     cfm = confusion_matrix(np.argmax(y_test, axis=1),
                            model.predict_classes(x_test),
                            labels=[i for i in range(y_count)]
@@ -92,11 +92,18 @@ print('\n')
 
 def test(x):
     namestem = x
-    testem = namestem
-    test_dummy = testem
-    test_dummy = [test_dummy]
-    test_dummy = cv.transform(test_dummy)
-    test_dummy = test_dummy.todense()
+    test_dummy = namestem
+    test_dummy = pd.DataFrame({"stem": [test_dummy]})
+    test_dummy["n4"] = ""
+    test_dummy["n4"] = test_dummy['stem'].str.strip().str[-4]
+    test_dummy["n3"] = ""
+    test_dummy["n3"] = test_dummy['stem'].str.strip().str[-3]
+    test_dummy["n2"] = ""
+    test_dummy["n2"] = test_dummy['stem'].str.strip().str[-2]
+    test_dummy["n1"] = ""
+    test_dummy["n1"] = test_dummy['stem'].str.strip().str[-1]
+    test_dummy = test_dummy.drop('stem', 1)
+    test_dummy = OneHotEncoder().transform(test_dummy)
     testmodel = model.predict_classes(test_dummy)
     print('prediction class for')
     print(namestem)
